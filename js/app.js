1c86dc019e43c0e3596329976542ec88f00449ed
AMel1323 <alves.melissa1@aluno.ifsp.edu.br>
2025-03-31 14:53:51 -0300

    modificacao

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+
+      const alunos = [
+        {
+          _id: 0,
+          nome: "chico melato",
+          notas: {
+            backend_1: [7,8, 9, 6],
+            frontend_2: [2, 2, 2, 2],
+            bancodados: [2, 2, 3, 3],
+            ferramentas: [3, 3, 3, 3],
+          },
+        },
+        {
+          _id: 1,
+          nome: "talita lima",
+          notas: {
+            backend_1: [4, 4, 4, 4],
+            frontend_2: [4, 4, 5, 5],
+            bancodados: [5, 5, 6, 6],
+            ferramentas: [7, 7, 8, 9],
+          },
+        },
+        {
+          _id: 1,
+          nome: "Fulano da silva",
+          notas: {
+            backend_1: [4, 4, 4, 4],
+            frontend_2: [4, 4, 5, 5],
+            bancodados: [5, 5, 6, 6],
+            ferramentas: [7, 7, 8, 9],
+          },
+        },
+      ];
+   
+ const AlunoService = new Aluno()
+
 alunos.forEach(aluno => {
     aluno.media = {}
     for(let materia in aluno.notas){
@@ -5,18 +41,59 @@ alunos.forEach(aluno => {
     }
 })
 
-console.log(alunos)
+// inserir no thead da tabela a lista de matérias
 
 const htmlHeader = document.createElement('tr')
 htmlHeader.innerHTML ='<td>Nome</td>'
 
 const htmlHeaderMaterias = Object.keys(alunos[0].notas).map(materia => {
-    console.log(materia)
+    
     return `<td>${materia}</td>`
 }).join('')
 
-console.log(htmlHeaderMaterias)
+
 htmlHeader.innerHTML += htmlHeaderMaterias
-console.log(htmlHeader)
+
 
 document.querySelector('[data-table-alunos] thead').appendChild(htmlHeader) 
+
+// inserir no tbody da tabela a lista de alunos e suas médias
+function render(){
+    document.querySelector('[data-table-alunos] tbody').innerHTML = ''
+ 
+ alunos.forEach(aluno => {
+    let htmlRow = document.createElement('tr')
+    htmlRow.innerHTML = `<td>${aluno.nome}</td>`
+    let htmlRowMaterias = Object.keys(aluno.media).map(materia => {
+        return`<td>${aluno.media[materia]}</td>`
+    }).join('')
+    htmlRow.innerHTML += htmlRowMaterias
+    document.querySelector('[data-table-alunos] tbody').appendChild(htmlRow)
+})
+}
+
+render ()
+// inserir aluno
+document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault()
+    const nome = document.getElementById('first_name').value
+    console.log(nome)
+    const newAluno = {
+        _id: 0,
+        nome, 
+        notas: {
+            backend_1:[6,6,6,6],
+            frontend_2: [7,7,7,7],
+            bancodados: [6,6,6,6],
+            ferramentas: [7,7,7,7],
+        },
+
+    }
+    newAluno.media = {}
+
+    for (let materia in newAluno.notas){
+        newAluno.media[materia] = average(...newAluno.notas[materia])
+    }
+    alunos.push(newAluno)
+    render()
+})
---
+++ js/services/Aluno.service.js
@@ -0,0 +1,40 @@
+class Alunoservice {
+    constructor () {
+        this.alunos = []
+    }
+
+add (aluno) {
+    if(!aluno instanceof AlunoModel) {
+        throw new Error('O aluno deve ser uma instância de AlunoModel')
+    }
+    this.alunos.push(aluno)
+}
+
+update(aluno) {
+    return aluno
+}
+
+searchById(id){
+    return this.alunos.find(aluno => aluno._id === id)
+}
+
+
+
+updateLocalStorage(){
+    let alunos = JSON.stringify(this.alunos)
+    localStorage.setItem('alunos', alunos)
+    }
+    
+    updateListAlunosFromLocalStorage() {
+    let local = localStorage.getItem('alunos')
+    let alunos = []
+    if(local){
+    alunos = JSON.parse(local)
+    }
+    alunos.forEach(aluno => {
+        this.add(new AlunoModel(aluno))
+    })
+    }
+
+    
+}  
